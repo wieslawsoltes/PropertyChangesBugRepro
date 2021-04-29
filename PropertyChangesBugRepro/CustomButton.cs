2c4766b678fc139497086b362c5bf80af7227003
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-29 23:33:55 +0200

    Move

---
+++ PropertyChangesBugRepro/CustomButton.cs
@@ -0,0 +1,50 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Layout;
+
+namespace PropertyChangesBugRepro
+{
+    [PseudoClasses(":horizontal", ":vertical")]
+    public class CustomButton : Button
+    {
+        public static readonly StyledProperty<bool> CustomPropertyProperty = 
+            AvaloniaProperty.Register<CustomButton, bool>(nameof(CustomProperty));
+
+        public static readonly StyledProperty<Orientation> IndicatorOrientationProperty =
+            AvaloniaProperty.Register<CustomButton, Orientation>(nameof(IndicatorOrientation), Orientation.Vertical);
+        
+        public bool CustomProperty
+        {
+            get => GetValue(CustomPropertyProperty);
+            set => SetValue(CustomPropertyProperty, value);
+        }
+        
+        public Orientation IndicatorOrientation
+        {
+            get => GetValue(IndicatorOrientationProperty);
+            set => SetValue(IndicatorOrientationProperty, value);
+        }
+        
+        public CustomButton()
+        {
+            UpdatePseudoClasses(IndicatorOrientation);
+        }
+        
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == IndicatorOrientationProperty)
+            {
+                UpdatePseudoClasses(change.NewValue.GetValueOrDefault<Orientation>());
+            }
+        }
+        
+        private void UpdatePseudoClasses(Orientation orientation)
+        {
+            PseudoClasses.Set(":horizontal", orientation == Orientation.Horizontal);
+            PseudoClasses.Set(":vertical", orientation == Orientation.Vertical);
+        }
+    }
+}
--- PropertyChangesBugRepro/MainWindow.axaml.cs
+++ PropertyChangesBugRepro/MainWindow.axaml.cs
@@ -1,55 +1,10 @@
 using System;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Metadata;
-using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 
 namespace PropertyChangesBugRepro
 {
-    [PseudoClasses(":horizontal", ":vertical")]
-    public class CustomButton : Button
-    {
-        public static readonly StyledProperty<bool> CustomPropertyProperty = 
-            AvaloniaProperty.Register<CustomButton, bool>(nameof(CustomProperty));
-
-        public static readonly StyledProperty<Orientation> IndicatorOrientationProperty =
-            AvaloniaProperty.Register<CustomButton, Orientation>(nameof(IndicatorOrientation), Orientation.Vertical);
-        
-        public bool CustomProperty
-        {
-            get => GetValue(CustomPropertyProperty);
-            set => SetValue(CustomPropertyProperty, value);
-        }
-        
-        public Orientation IndicatorOrientation
-        {
-            get => GetValue(IndicatorOrientationProperty);
-            set => SetValue(IndicatorOrientationProperty, value);
-        }
-        
-        public CustomButton()
-        {
-            UpdatePseudoClasses(IndicatorOrientation);
-        }
-        
-        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
-        {
-            base.OnPropertyChanged(change);
-
-            if (change.Property == IndicatorOrientationProperty)
-            {
-                UpdatePseudoClasses(change.NewValue.GetValueOrDefault<Orientation>());
-            }
-        }
-        
-        private void UpdatePseudoClasses(Orientation orientation)
-        {
-            PseudoClasses.Set(":horizontal", orientation == Orientation.Horizontal);
-            PseudoClasses.Set(":vertical", orientation == Orientation.Vertical);
-        }
-    }
-
     public class MainWindow : Window
     {
         public MainWindow()
